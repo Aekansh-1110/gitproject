8133d0d0a9f9fbd527e43907a607f8d92b032a15
Aekansh-1110 <ashwinimunde4793@gmail.com>
2024-04-14 23:02:54 +0530

    commit2

--- src/gitproject/git.java
+++ src/gitproject/git.java
@@ -3,7 +3,9 @@ package gitproject;
 public class git {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
 
 	}
 
